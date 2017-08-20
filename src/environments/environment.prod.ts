058d177343975e401ab6b6a8d3597f0d294e69f6
Amarpreet Singh <amarpreet2209@gmail.com>
2017-08-20 19:04:08 +0530

    Part 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyDEWXokHK14aU5WO-rDrl0vMy_3ZNiz_-8',
+    authDomain: 'chat-app-bcff3.firebaseapp.com',
+    databaseURL: 'https://chat-app-bcff3.firebaseio.com',
+    projectId: 'chat-app-bcff3',
+    storageBucket: 'chat-app-bcff3.appspot.com',
+    messagingSenderId: '303031588660'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyDEWXokHK14aU5WO-rDrl0vMy_3ZNiz_-8',
+    authDomain: 'chat-app-bcff3.firebaseapp.com',
+    databaseURL: 'https://chat-app-bcff3.firebaseio.com',
+    projectId: 'chat-app-bcff3',
+    storageBucket: 'chat-app-bcff3.appspot.com',
+    messagingSenderId: '303031588660'
+  }
 };
